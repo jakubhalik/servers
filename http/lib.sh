aa5cfb026f72f1cfee220f3893f06cb997b311e0
Jakub Halik <oranzovydzusaja@proton.me>
2025-11-22 20:31:54 +0100

    absolute fugin legendarium

--- http/lib.sh
+++ http/lib.sh
@@ -1,10 +1,27 @@
 host_web_text_that_will_stop_being_hosted_after_being_fetched_once() {
-  test $(echo $@) || (echo "Usage: $0 <plaintext to put on the website> <port to host on>" && exit 1)
+  test $(echo $2) || (echo "Usage: $0 <plaintext to put on the website> <port to host on>" && exit 1)
   (
     echo "HTTP/1.1 200 OK"
     echo "Content-Length: $(echo $1|wc -c)"
     echo ""
     echo "$1"
   ) | nc -l -p $2
+}
+
+get_server () {
+  test $(echo $2) || (echo "Usage: $0 <plaintext to put on the website> <port to host on>" && exit 1)
+  dash -c '
+    msg="$1"
+    port="$2"
+    while true; do
+      {
+        printf "HTTP/1.1 200 OK\r\n"
+        printf "Content-Type: text/plain\r\n"
+        printf "Content-Length: %s\r\n" "$(printf "%s" "$msg" | wc -c)"
+        printf "\r\n"
+        printf "%s" "$msg"
+      } | nc -l -p "$port"
+    done
+  ' dash "$1" "$2"
 }
 
