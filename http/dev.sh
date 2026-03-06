53c759534faeb167e455c1ceccd7f2331ce2c8c0
Jakub Halik <oranzovydzusaja@proton.me>
2026-03-06 11:15:12 +0100

    utils

--- http/dev.sh
+++ http/dev.sh
@@ -2,9 +2,12 @@ clc() {
   curl localhost:$1
 }
 
-clc_test() {
+c_test() {
   test $(echo $1) || (echo "Usage: $0 <port to be forever curling>" && exit 1)
-  seq 1 99999999999999999999999999999999999999999999999999999999999999999999999|iter dash -c 'timeout 0.1s curl localhost:$1; echo {}' dash $1
+  seq 1 99999999999999999999999999999999999999999999999999999999999999999999999|iter dash -c 'timeout 0.1s curl $1; echo {}' dash $1
+}
+clc_test() {
+  c_test localhost:$1
 }
 
 clct() {clc_test $@}
